816dc70ec40152add7f8974323e806877591e8eb
Allen D. Householder <adh@cert.org>
2014-01-03 13:24:31 -0500

    create a stateful timer

---
+++ src/certfuzz/test/fuzztools/test_state_timer.py
@@ -0,0 +1,43 @@
+'''
+Created on Jan 3, 2014
+
+@author: adh
+'''
+import unittest
+from certfuzz.fuzztools import state_timer
+import time
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.st = state_timer.StateTimer()
+
+    def tearDown(self):
+        pass
+
+    def test_enter_state(self):
+        st = self.st
+
+        self.assertEqual(0, len(st.timers))
+        st.enter_state('alpha')
+        self.assertEqual(1, len(st.timers))
+        time.sleep(1)
+        st.enter_state(None)
+        self.assertEqual(1, len(st.timers))
+        self.assertAlmostEqual(1.0, st.time_in('alpha'), 1)
+
+        st.enter_state('alpha')
+        self.assertEqual(1, len(st.timers))
+        time.sleep(1)
+        st.enter_state('beta')
+        self.assertEqual(2, len(st.timers))
+        self.assertAlmostEqual(2.0, st.time_in('alpha'), 1)
+        self.assertEqual(0.0, st.time_in('beta'))
+        time.sleep(1)
+        st.enter_state(None)
+        self.assertAlmostEqual(2.0, st.time_in('alpha'), 1)
+        self.assertAlmostEqual(1.0, st.time_in('beta'), 1)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
