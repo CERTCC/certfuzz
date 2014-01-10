2f0fe9ccd391cbc760b8d881a0988d3ed75f3bfb
Allen D. Householder <adh@cert.org>
2014-01-10 13:50:31 -0500

    move scoring errors to their own module

--- src/certfuzz/scoring/errors.py
+++ src/certfuzz/scoring/errors.py
@@ -5,14 +5,14 @@ Created on Feb 22, 2013
 '''
 from certfuzz.errors import CERTFuzzError
 
+
 class ScoringError(CERTFuzzError):
     pass
+
 
 class ScorableSetError(ScoringError):
     pass
 
-class EmptySetError(ScorableSetError):
-    pass
 
-class ScorableThingError(ScoringError):
+class EmptySetError(ScorableSetError):
     pass
