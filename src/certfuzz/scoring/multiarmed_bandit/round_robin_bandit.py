244bd77b8380fed4372307aad404ab1fd14b1207
Allen D. Householder <adh@cert.org>
2014-01-10 10:52:44 -0500

    add round robin bandit (refactored out of base class in previous commit)

---
+++ src/certfuzz/scoring/multiarmed_bandit/round_robin_bandit.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 10, 2014
+
+@author: adh
+'''
+import itertools
+from certfuzz.scoring.multiarmed_bandit.multiarmed_bandit_base import MultiArmedBanditBase
+
+
+class RoundRobinMultiArmedBandit(MultiArmedBanditBase):
+    def __iter__(self):
+        '''
+        Implements a simple round robin iterator
+        '''
+        return itertools.cycle(self.things.values())
