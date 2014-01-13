70b59944a9dba0c69a9209bd81da85351051fab3
Allen D. Householder <adh@cert.org>
2014-01-13 13:02:13 -0500

    add empty test harnesses

---
+++ src/certfuzz/test/bff/test_linux.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 13, 2014
+
+@author: adh
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
