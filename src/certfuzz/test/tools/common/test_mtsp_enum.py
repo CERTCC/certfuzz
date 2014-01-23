2366485bf9daeaca12e6e89014d4567ac9a92fca
Allen D. Householder <adh@cert.org>
2014-01-23 10:19:41 -0500

    move tests into the right location

--- src/certfuzz/test/tools/common/test_mtsp_enum.py
+++ src/certfuzz/test/tools/common/test_mtsp_enum.py
@@ -1,26 +1,21 @@
 '''
-Created on Apr 10, 2012
+Created on Jan 23, 2014
 
 @organization: cert.org
 '''
-
 import unittest
 
 
 class Test(unittest.TestCase):
 
-
     def setUp(self):
         pass
-
 
     def tearDown(self):
         pass
 
-
     def testName(self):
         pass
-
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
