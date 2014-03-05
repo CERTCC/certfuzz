1267c6ea1e12c5b099d51f319144b53f67af1bf6
Allen D. Householder <adh@cert.org>
2014-03-05 09:26:10 -0500

    add debug logging

--- build/dist/windows/windows_build2.py
+++ build/dist/windows/windows_build2.py
@@ -5,10 +5,12 @@ Created on Dec 9, 2013
 '''
 import os
 import shutil
-
+import logging
 import subprocess
 
 from ..build_base2 import Build
+
+logger = logging.getLogger(__name__)
 
 basedir = os.path.dirname(__file__)
 
@@ -21,17 +23,21 @@ class WindowsBuild(Build):
         '''
         Builds a Windows Installer
         '''
+        logger.debug('Import buildnsi')
         from .nsis import buildnsi
 
         # Copy files required by nsis
         for f in ['cert.ico', 'EnvVarUpdate.nsh', 'vmwarning.txt']:
             src = os.path.join(basedir, 'nsis', f)
+            logger.debug('Copy %s -> %s', src, self.build_dir)
             shutil.copy(src, self.build_dir)
 
         nsifile = os.path.join(self.build_dir, 'foe2.nsi')
+        logger.debug('nsi file is %s', nsifile)
 
         # generate the nsi file
         buildnsi.main(version_string="02.01.00.99", outfile=nsifile, build_dir=self.build_dir)
 
         # invoke makensis on the file we just made
+        logger.debug('invoking makensis on %s', nsifile)
         subprocess.call(['makensis', nsifile])
