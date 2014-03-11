53c2a834e73db33b01f510dbb694d485301a2641
Allen D. Householder <adh@cert.org>
2014-03-11 16:31:03 -0400

    pep8 cleanup

--- src/certfuzz/helpers/misc.py
+++ src/certfuzz/helpers/misc.py
@@ -11,20 +11,25 @@ import logging
 
 my_os = platform.system()
 
+
 def quoted(string_to_wrap):
     return '"%s"' % string_to_wrap
 
+
 def print_dict(d):
     pprint(d)
+
 
 def check_os_compatibility(expected_os, module_name=__name__):
     if not my_os == expected_os:
         template = 'Module %s is incompatible with %s (%s expected)'
         raise ImportError(template % (module_name, my_os, expected_os))
 
+
 def random_str(length=1):
     chars = string.ascii_letters + string.digits
     return ''.join([random.choice(chars) for dummy in xrange(length)])
+
 
 def bitswap(input_byte):
     bits = [1, 2, 4, 8, 16, 32, 64, 128]
@@ -45,6 +50,7 @@ def bitswap(input_byte):
         if input_byte & x:
             output_byte |= y
     return output_byte
+
 
 def log_object(obj, logger, level=logging.DEBUG):
     for l in pformat(obj.__dict__).splitlines():
