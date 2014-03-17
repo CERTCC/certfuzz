332e1504b1c24572b79887c57aa9c78befea9e8d
Allen D. Householder <adh@cert.org>
2014-03-17 13:02:54 -0400

    if path is a link, then exc.errno == errno.EEXIST but not os.path.isdir(path)

--- src/certfuzz/fuzztools/filetools.py
+++ src/certfuzz/fuzztools/filetools.py
@@ -54,7 +54,7 @@ def mkdir_p(path):
         os.makedirs(path)
     except OSError as exc:
         # if the dir already exists, just move along
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
+        if exc.errno == errno.EEXIST:
             return True
         else:
             raise
