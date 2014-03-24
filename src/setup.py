968ef17fa93138375b501008c3008d46cfccd181
Allen D. Householder <adh@cert.org>
2014-03-24 16:16:21 -0400

    revamp setup.py for bff 3.0

--- src/setup.py
+++ src/setup.py
@@ -4,10 +4,27 @@ Created on Jul 10, 2012
 @organization: cert.org
 '''
 from setuptools import setup, find_packages
+import platform
+
+
+def _bff_for_platform():
+    parts = ['certfuzz', 'bff']
+    _platform = platform.system()
+    if _platform == 'Windows':
+        parts.append('windows')
+    else:
+        # actually covers linux and osx
+        parts.append('linux')
+
+    module = '.'.join(parts)
+    return module
+
+_bff_main = 'bff = {}:main'.format(_bff_for_platform())
+
 
 setup(name="CERT_Basic_Fuzzing_Framework",
-      version="1.0-android",
-      description="A fuzzing framework for Android, based on BFF 2.7",
+      version="3.0a",
+      description="CERT Basic Fuzzing Framework 3.0",
       author="CERT",
       author_email="cert@cert.org",
       url="http://www.cert.org",
@@ -17,19 +34,30 @@ setup(name="CERT_Basic_Fuzzing_Framework",
       packages=find_packages(where='.'),
       install_requires=[
                         'pyyaml',
-                        'couchdb',
+#                        'couchdb',
                         'numpy',
+                        'matplotlib',
                         ],
-      scripts=['scripts/start_bff_android.sh',
-               'scripts/reset_bff_android.sh',
-               'scripts/ubufuzz_first_time_setup.sh',
+      scripts=[
+#            'scripts/start_bff_android.sh',
+#            'scripts/reset_bff_android.sh',
+#            'scripts/ubufuzz_first_time_setup.sh',
                ],
-      entry_points={'console_scripts': [
-            'bff_avd_mgr = certfuzz.android.avd_mgr.main:main',
-            'bff_android = certfuzz.bff.android:main',
-            'bff_avd_cloner = certfuzz.android.celery.avd_mgr.cloner:main',
-            'bff_apk_dumper = certfuzz.tools.android.apk_dumper:main',
-            'config_init = certfuzz.tools.android.config_tools:main'
-            ]},
+      entry_points={
+                    'console_scripts': [
+#                        # bff for android
+#                        'bff_avd_mgr = certfuzz.android.avd_mgr.main:main',
+#                        'bff_android = certfuzz.android.controller.bff_android:main',
+#                        'bff_avd_cloner = certfuzz.android.celery.avd_mgr.cloner:main',
+#                        'bff_apk_dumper = certfuzz.android.tools.apk_dumper:main',
+#                        'config_init = certfuzz.android.tools.config_tools:main',
+                        # bff for linux, osx, windows,
+                       _bff_main,
+                        ]
+                    },
       include_package_data=True,
+      license='See LICENSE.txt',
+      data_files=[
+                    ('', ['LICENSE.txt'])
+                    ]
       )
