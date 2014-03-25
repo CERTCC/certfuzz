d3a26d11d8ebce011bdb88aa4ac2a9229244a740
Allen D. Householder <adh@cert.org>
2014-03-25 15:08:32 -0400

    get rid of requirements.txt. Just build a plain vanilla virtualenv that inherits from the system

--- setup_env/deploy_virtualenv.sh
+++ setup_env/deploy_virtualenv.sh
@@ -2,4 +2,4 @@
 
 ENV=bff.env
 
-virtualenv --system-site-packages --distribute $ENV && source $ENV/bin/activate && pip install -r requirements.txt
+virtualenv --system-site-packages --distribute $ENV
