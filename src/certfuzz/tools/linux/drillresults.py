ec5fc8b8ce94c30b79b8ae4d01aced32638185eb
Allen D. Householder <adh@cert.org>
2014-06-30 10:29:28 -0400

    refactor out common methods
    readfile
    carve
    carve2
    score_reports

--- src/certfuzz/tools/linux/drillresults.py
+++ src/certfuzz/tools/linux/drillresults.py
@@ -8,6 +8,9 @@ import binascii
 import re
 from optparse import OptionParser
 
+from certfuzz.tools.common.drillresults import readfile, carve, carve2, \
+    score_reports
+
 regex = {
         'gdb_report': re.compile(r'.+.gdb$'),
         'current_instr': re.compile(r'^=>\s(0x[0-9a-fA-F]+)(.+)?:\s+(\S.+)'),
@@ -122,15 +125,6 @@ def fixefabug(reporttext, instraddr, faultaddr):
     return faultaddr
 
 
-def readfile(textfile):
-    '''
-    Read text file
-    '''
-    f = open(textfile, 'r')
-    text = f.read()
-    return text
-
-
 def readbinfile(textfile):
     '''
     Read binary file
@@ -138,33 +132,6 @@ def readbinfile(textfile):
     f = open(textfile, 'rb')
     text = f.read()
     return text
-
-
-def carve(string, token1, token2):
-    startindex = string.find(token1)
-    if startindex == -1:
-        # can't find token1
-        return ""
-    startindex = startindex + len(token1)
-    endindex = string.find(token2, startindex)
-    if endindex == -1:
-        # can't find token2
-        return ""
-    return string[startindex:endindex]
-
-
-# Todo: fix this up.  Was added to bring gdb support
-def carve2(string):
-    delims = [("Exception Faulting Address: ", "\n"),
-              ("si_addr:$2 = (void *)", "\n")]
-    for token1, token2 in delims:
-        startindex = string.find(token1)
-        if startindex == -1:
-            # can't find token1
-            continue
-        startindex = startindex + len(token1)
-        endindex = string.find(token2, startindex)
-        return string[startindex:endindex]
 
 
 def getexnum(reporttext):
@@ -208,14 +175,11 @@ def getinstr(reporttext, instraddr):
     '''
     Find the disassembly line for the current (crashing) instruction
     '''
-    instructionline = ''
-
     for line in reporttext.splitlines():
         n = re.match(regex['current_instr'], line)
         if n:
-            instructionline = n.group(3)
-            break
-    return instructionline
+            return n.group(3)
+    return ''
 
 
 def formataddr(faultaddr):
@@ -243,18 +207,6 @@ def formataddr(faultaddr):
             faultaddr = faultaddr.zfill(8)
 
     return faultaddr
-
-
-def ratchetscore(crasher, score):
-    '''
-    Only allow a crasher to increase score
-    '''
-    try:
-        currentscore = scoredcrashes[crasher]
-    except KeyError:
-        currentscore = 100
-    if score < currentscore:
-        scoredcrashes[crasher] = score
 
 
 def fixefaoffset(instructionline, faultaddr):
@@ -333,10 +285,9 @@ def checkreport(reportfile, crasherfile, crash_hash):
     if shortdesc:
         # Set !exploitable Short Description for the exception
         crashid['exceptions'][exceptionnum]['shortdesc'] = shortdesc
-        if shortdesc in re_set:
-            # Flag the entire crash ID as really exploitable if this is a good
-            # exception
-            crashid['reallyexploitable'] = True
+        # Flag the entire crash ID as really exploitable if this is a good
+        # exception
+        crashid['reallyexploitable'] = shortdesc in re_set
 
     if not os.path.isfile(crasherfile):
         # Can't find the crasher file
@@ -429,72 +380,6 @@ def parsegdbs(gdblist):
         checkreport(gdb['gdbfile'], gdb['crasherfile'], gdb['crash_hash'])
 
 
-def score_reports():
-    # Assign a ranking to each crash report.  The lower the rank,
-    # the higher the exploitability
-    if results:
-        print "--- Interesting crashes ---\n"
-        # For each of the crash ids in the results dictionary, apply ranking
-        for crasher in results:
-            try:
-                if results[crasher]['reallyexploitable'] == True:
-                    # The crash summery is a very interesting one
-                    for exception in results[crasher]['exceptions']:
-                        module = results[crasher]['exceptions'][exception]['pcmodule']
-                        if module == 'unloaded' and not ignorejit:
-                            # EIP is not in a loaded module
-                            ratchetscore(crasher, 20)
-                        if results[crasher]['exceptions'][exception]['shortdesc'] in re_set:
-                            efa = '0x' + results[crasher]['exceptions'][exception]['efa']
-                            if results[crasher]['exceptions'][exception]['EIF']:
-                                # The faulting address pattern is in the fuzzed file
-                                if '0x000000' in efa:
-                                    # Faulting address is near null
-                                    ratchetscore(crasher, 30)
-                                elif '0x0000' in efa:
-                                    # Faulting address is somewhat near null
-                                    ratchetscore(crasher, 20)
-                                elif '0xffff' in efa:
-                                    # Faulting address is likely a negative number
-                                    ratchetscore(crasher, 20)
-                                else:
-                                    # Faulting address has high entropy.  Most exploitable.
-                                    ratchetscore(crasher, 10)
-                            else:
-                                # The faulting address pattern is not in the fuzzed file
-                                ratchetscore(crasher, 40)
-                else:
-                    # The crash summary isn't necessarily interesting
-                    for exception in results[crasher]['exceptions']:
-                        efa = '0x' + results[crasher]['exceptions'][exception]['efa']
-                        module = results[crasher]['exceptions'][exception]['pcmodule']
-                        if module == 'unloaded' and not ignorejit:
-                            ratchetscore(crasher, 20)
-                        elif module.lower() == 'ntdll.dll' or 'msvcr' in module.lower():
-                            #likely heap corruption.  Exploitable, but difficult
-                            ratchetscore(crasher, 45)
-                        elif '0x00120000' in efa or '0x00130000' in efa or '0x00140000' in efa:
-                            #non-continued potential stack buffer overflow
-                            ratchetscore(crasher, 40)
-                        elif results[crasher]['exceptions'][exception]['EIF']:
-                            # The faulting address pattern is in the fuzzed file
-                            if '0x000000' in efa:
-                                # Faulting address is near null
-                                ratchetscore(crasher, 70)
-                            elif '0x0000' in efa:
-                                # Faulting address is somewhat near null
-                                ratchetscore(crasher, 60)
-                            elif '0xffff' in efa:
-                                # Faulting address is likely a negative number
-                                ratchetscore(crasher, 60)
-                            else:
-                                # Faulting address has high entropy.
-                                ratchetscore(crasher, 50)
-            except KeyError:
-                #print "Error scoring crash %s" % crasher
-                continue
-
-
 def printreport():
     sorted_crashes = sorted(scoredcrashes.iteritems(), key=lambda(k, v): (v, k))
 
@@ -541,7 +426,7 @@ def main():
         tld = 'crashers'
     gdblist = findgdbs(tld)
     parsegdbs(gdblist)
-    score_reports()
+    score_reports(results, scoredcrashes, ignorejit)
     printreport()
 
 if __name__ == '__main__':
