cb09c714ed10e08fdec0b41bda5593bf45365ed1
Allen D. Householder <adh@cert.org>
2014-07-01 08:15:02 -0400

    move methods into objects
    linux:
    checkreport
    
    windows:
    check_64bit
    pc_in_mapped_address
    format_addr
    checkreport
    find_dbg_output
    
    common:
    score_reports
    print_reports
    score_crasher
    load_cached
    check_dirs

--- src/certfuzz/tools/common/drillresults.py
+++ src/certfuzz/tools/common/drillresults.py
@@ -6,6 +6,10 @@ Created on Jun 30, 2014
 import os
 import cPickle as pickle
 import abc
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 registers = ('eax', 'ecx', 'edx', 'ebx', 'esp', 'ebp', 'esi',
              'edi', 'eip')
@@ -60,82 +64,8 @@ def is_number(s):
     except ValueError:
         return False
 
-
-def score_crasher(crasher, details, ignorejit, re_set):
-    scores = [100]
-    if details['reallyexploitable'] == True:
-    # The crash summary is a very interesting one
-        for exception in details['exceptions']:
-            module = details['exceptions'][exception]['pcmodule']
-            if module == 'unloaded' and not ignorejit:
-                # EIP is not in a loaded module
-                scores.append(20)
-            if details['exceptions'][exception]['shortdesc'] in re_set:
-                efa = '0x' + details['exceptions'][exception]['efa']
-                if details['exceptions'][exception]['EIF']:
-                # The faulting address pattern is in the fuzzed file
-                    if '0x000000' in efa:
-                        # Faulting address is near null
-                        scores.append(30)
-                    elif '0x0000' in efa:
-                        # Faulting address is somewhat near null
-                        scores.append(20)
-                    elif '0xffff' in efa:
-                        # Faulting address is likely a negative number
-                        scores.append(20)
-                    else:
-                        # Faulting address has high entropy.  Most exploitable.
-                        scores.append(10)
-                else:
-                    # The faulting address pattern is not in the fuzzed file
-                    scores.append(40)
-
-    else:
-        # The crash summary isn't necessarily interesting
-        for exception in details['exceptions']:
-            efa = '0x' + details['exceptions'][exception]['efa']
-            module = details['exceptions'][exception]['pcmodule']
-            if module == 'unloaded' and not ignorejit:
-                scores.append(20)
-            elif module.lower() == 'ntdll.dll' or 'msvcr' in module.lower():
-                # likely heap corruption.  Exploitable, but difficult
-                scores.append(45)
-            elif '0x00120000' in efa or '0x00130000' in efa or '0x00140000' in efa:
-                # non-continued potential stack buffer overflow
-                scores.append(40)
-            elif details['exceptions'][exception]['EIF']:
-            # The faulting address pattern is in the fuzzed file
-                if '0x000000' in efa:
-                    # Faulting address is near null
-                    scores.append(70)
-                elif '0x0000' in efa:
-                    # Faulting address is somewhat near null
-                    scores.append(60)
-                elif '0xffff' in efa:
-                    # Faulting address is likely a negative number
-                    scores.append(60)
-                else:
-                    # Faulting address has high entropy.
-                    scores.append(50)
-    return min(scores)
-
-
-def score_reports(results, crashscores, ignorejit, re_set):
-    # Assign a ranking to each crash report.  The lower the rank, the higher
-    # the exploitability
-    if results:
-        print "--- Interesting crashes ---\n"
-        # For each of the crash ids in the results dictionary, apply ranking
-        for crasher in results:
-            try:
-                crashscores[crasher] = score_crasher(crasher, results[crasher], ignorejit, re_set)
-            except KeyError:
-                print "Error scoring crash %s" % crasher
-                continue
-
-
-def print_crash_report(crasher, score, details, ignorejit):
-    print '\n%s - Exploitability rank: %s' % (crasher, score)
+def print_crash_report(crash_key, score, details, ignorejit):
+    print '\n%s - Exploitability rank: %s' % (crash_key, score)
     print 'Fuzzed file: %s' % details['fuzzedfile']
     for exception in details['exceptions']:
         shortdesc = details['exceptions'][exception]['shortdesc']
@@ -157,28 +87,9 @@ def print_crash_report(crasher, score, details, ignorejit):
 def print_report(scoredcrashes, results, ignorejit):
     sorted_crashes = sorted(scoredcrashes.iteritems(), key=lambda(k, v): (v, k))
 
-    for crashes in sorted_crashes:
-        crasher = crashes[0]
-        score = crashes[1]
-        details = results[crasher]
-        print_crash_report(crasher, score, details)
-
-
-def load_cached(pkl_filename):
-    try:
-        with open(pkl_filename, 'rb') as pkl_file:
-            return pickle.load(pkl_file)
-    except IOError:
-        # No cached results
-        pass
-
-
-def cache_results(pkl_filename, results):
-    pkldir = os.path.dirname(pkl_filename)
-    if not os.path.exists(pkldir):
-        os.makedirs(pkldir)
-    with open(pkl_filename, 'wb') as pkl_file:
-        pickle.dump(results, pkl_file, -1)
+    for crash_key, score in sorted_crashes:
+        details = results[crash_key]
+        print_crash_report(crash_key, score, details)
 
 
 class DrillResultsError(Exception):
@@ -200,29 +111,22 @@ class ResultDriller(object):
         self.pickle_file = os.path.join('fuzzdir', 'drillresults.pkl')
         self.cached_results = None
         self.dbg_out = []
+        self.results = {}
+        self.scoredcrashes = {}
+
+        self._64bit_debugger = False
+
+        self.re_set = set(self.really_exploitable)
+
+    @abc.abstractproperty
+    def really_exploitable(self):
+        return []
 
     def __enter__(self):
         return self
 
     def __exit__(self, etype, value, traceback):
         return
-
-    def _check_dirs(self):
-        check_dirs = [self.base_dir, 'results', 'crashers']
-        for d in check_dirs:
-            if os.path.isdir(d):
-                self.tld = d
-                return
-        # if you got here, none of them exist
-        raise DrillResultsError('None of {} appears to be a dir'.format(check_dirs))
-
-    def load_cached(self):
-        try:
-            with open(self.pkl_filename, 'rb') as pkl_file:
-                self.cached_results = pickle.load(pkl_file)
-        except IOError:
-            # No cached results
-            pass
 
     @abc.abstractmethod
     def check_64bit(self, reporttext):
@@ -249,9 +153,103 @@ class ResultDriller(object):
     def _check_report(self, dbg_file, crash_file, crash_hash, cached_results):
         pass
 
+    def _check_dirs(self):
+        check_dirs = [self.base_dir, 'results', 'crashers']
+        for d in check_dirs:
+            if os.path.isdir(d):
+                self.tld = d
+                return
+        # if you got here, none of them exist
+        raise DrillResultsError('None of {} appears to be a dir'.format(check_dirs))
+
+    def load_cached(self):
+        if self.force:
+            logger.info('--force option used, ignoring cached results')
+            return
+
+        try:
+            with open(self.pkl_filename, 'rb') as pkl_file:
+                self.cached_results = pickle.load(pkl_file)
+        except IOError:
+            # No cached results
+            pass
+
     def check_reports(self):
         for dbg_file, crash_file, crash_hash in self.dbg_out:
-            self.check_report(dbg_file, crash_file, crash_hash, self.cached_results)
+            self._check_report(dbg_file, crash_file, crash_hash, self.cached_results)
+
+    def _score_crasher(self, details):
+        scores = [100]
+        if details['reallyexploitable'] == True:
+        # The crash summary is a very interesting one
+            for exception in details['exceptions']:
+                module = details['exceptions'][exception]['pcmodule']
+                if module == 'unloaded' and not self.ignorejit:
+                    # EIP is not in a loaded module
+                    scores.append(20)
+                if details['exceptions'][exception]['shortdesc'] in self.re_set:
+                    efa = '0x' + details['exceptions'][exception]['efa']
+                    if details['exceptions'][exception]['EIF']:
+                    # The faulting address pattern is in the fuzzed file
+                        if '0x000000' in efa:
+                            # Faulting address is near null
+                            scores.append(30)
+                        elif '0x0000' in efa:
+                            # Faulting address is somewhat near null
+                            scores.append(20)
+                        elif '0xffff' in efa:
+                            # Faulting address is likely a negative number
+                            scores.append(20)
+                        else:
+                            # Faulting address has high entropy.  Most exploitable.
+                            scores.append(10)
+                    else:
+                        # The faulting address pattern is not in the fuzzed file
+                        scores.append(40)
+        else:
+            # The crash summary isn't necessarily interesting
+            for exception in details['exceptions']:
+                efa = '0x' + details['exceptions'][exception]['efa']
+                module = details['exceptions'][exception]['pcmodule']
+                if module == 'unloaded' and not self.ignorejit:
+                    scores.append(20)
+                elif module.lower() == 'ntdll.dll' or 'msvcr' in module.lower():
+                    # likely heap corruption.  Exploitable, but difficult
+                    scores.append(45)
+                elif '0x00120000' in efa or '0x00130000' in efa or '0x00140000' in efa:
+                    # non-continued potential stack buffer overflow
+                    scores.append(40)
+                elif details['exceptions'][exception]['EIF']:
+                # The faulting address pattern is in the fuzzed file
+                    if '0x000000' in efa:
+                        # Faulting address is near null
+                        scores.append(70)
+                    elif '0x0000' in efa:
+                        # Faulting address is somewhat near null
+                        scores.append(60)
+                    elif '0xffff' in efa:
+                        # Faulting address is likely a negative number
+                        scores.append(60)
+                    else:
+                        # Faulting address has high entropy.
+                        scores.append(50)
+        return min(scores)
+
+    def score_reports(self, crashscores, ignorejit, re_set):
+        # Assign a ranking to each crash report.  The lower the rank, the higher
+        # the exploitability
+        if self.results:
+            print "--- Interesting crashes ---\n"
+            # For each of the crash ids in the results dictionary, apply ranking
+            for crash_key, crash_details in self.results.iteritems():
+                try:
+                    crashscores[crash_key] = self._score_crasher(crash_details)
+                except KeyError:
+                    print "Error scoring crash %s" % crash_key
+                    continue
+
+    def print_reports(self):
+        pass
 
     def cache_results(self):
         pkldir = os.path.dirname(self.pkl_filename)
@@ -260,13 +258,9 @@ class ResultDriller(object):
         with open(self.pkl_filename, 'wb') as pkl_file:
             pickle.dump(self.results, pkl_file, -1)
 
-
     def drill_results(self):
         self._check_dirs()
-
-        if not self.force:
-            self.load_cached()
-
+        self.load_cached()
         self.find_dbg_output()
         self.check_reports()
         self.score_reports()
