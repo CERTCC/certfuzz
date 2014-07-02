9bc3eaeef234fb942e720f44f02e670959bb958e
Allen D. Householder <adh@cert.org>
2014-07-02 09:50:47 -0400

    refactor functions into methods

--- src/certfuzz/tools/linux/drillresults.py
+++ src/certfuzz/tools/linux/drillresults.py
@@ -35,167 +35,6 @@ regex = {
         }
 
 
-def pc_in_mapped_address(reporttext, instraddr):
-    '''
-    Check if the instruction pointer is in a loaded module
-    '''
-    if not instraddr:
-        # The gdb file doesn't have anything in it that'll tell us
-        # where the PC is.
-        return ''
-#    print 'checking if %s is mapped...' % instraddr
-    mapped_module = 'unloaded'
-
-    instraddr = int(instraddr, 16)
-#    print 'instraddr: %d' % instraddr
-    for line in reporttext.splitlines():
-        #print 'checking: %s for %s' % (line,regex['mapped_frame'])
-        n = re.search(regex['mapped_frame'], line)
-        if n:
-#            print '*** found mapped address regex!'
-            # Strip out backticks present on 64-bit systems
-            begin_address = int(n.group(1).replace('`', ''), 16)
-            end_address = int(n.group(2).replace('`', ''), 16)
-            if begin_address < instraddr < end_address:
-                mapped_module = n.group(4)
-                #print 'mapped_module: %s' % mapped_module
-        else:
-            # [vdso] still counts as a mapped module
-            n = re.search(regex['vdso'], line)
-            if n:
-                begin_address = int(n.group(1).replace('`', ''), 16)
-                end_address = int(n.group(2).replace('`', ''), 16)
-                if begin_address < instraddr < end_address:
-                    mapped_module = '[vdso]'
-
-    return mapped_module
-
-
-def fix_efa_bug(reporttext, instraddr, faultaddr):
-    '''
-    !exploitable often reports an incorrect EFA for 64-bit targets.
-    If we're dealing with a 64-bit target, we can second-guess the reported EFA
-    '''
-    instructionline = get_instr(reporttext, instraddr)
-    if not instructionline:
-        return faultaddr
-    ds = carve(instructionline, "ds:", "=")
-    if ds:
-        faultaddr = ds.replace('`', '')
-    return faultaddr
-
-
-def get_ex_num(reporttext):
-    '''
-    Get the exception number by counting the number of continues
-    '''
-    exception = 0
-    for line in reporttext.splitlines():
-        n = re.match(regex['dbg_prompt'], line)
-        if n:
-            cdbcmd = n.group(1)
-            cmds = cdbcmd.split(';')
-            for cmd in cmds:
-                if cmd == 'g':
-                    exception = exception + 1
-    return exception
-
-
-def get_instr_addr(reporttext):
-    '''
-    Find the address for the current (crashing) instruction
-    '''
-    instraddr = None
-    for line in reporttext.splitlines():
-        #print 'checking: %s' % line
-        n = re.match(regex['current_instr'], line)
-        if n:
-            instraddr = n.group(1)
-            #print 'Found instruction address: %s' % instraddr
-    if not instraddr:
-        for line in reporttext.splitlines():
-            #No disassembly. Resort to frame 0 address
-            n = re.match(regex['frame0'], line)
-            if n:
-                instraddr = n.group(1)
-                #print 'Found instruction address: %s' % instraddr
-    return instraddr
-
-
-def get_instr(reporttext, instraddr):
-    '''
-    Find the disassembly line for the current (crashing) instruction
-    '''
-    rgx = regex['current_instr']
-    for line in reporttext.splitlines():
-        n = rgx.match(line)
-        if n:
-            return n.group(3)
-    return ''
-
-
-def format_addr(faultaddr, _64bit_debugger):
-    '''
-    Format a 64- or 32-bit memory address to a fixed width
-    '''
-
-    if not faultaddr:
-        return
-    else:
-        faultaddr = faultaddr.strip()
-    faultaddr = faultaddr.replace('0x', '')
-
-    if _64bit_debugger:
-        # Due to a bug in !exploitable, the Exception Faulting Address is
-        # often wrong with 64-bit targets
-        if len(faultaddr) < 10:
-            # pad faultaddr
-            faultaddr = faultaddr.zfill(16)
-    else:
-        if len(faultaddr) > 10:  # 0x12345678 = 10 chars
-            faultaddr = faultaddr[-8:]
-        elif len(faultaddr) < 10:
-            # pad faultaddr
-            faultaddr = faultaddr.zfill(8)
-
-    return faultaddr
-
-
-def fix_efa_offset(instructionline, faultaddr, _64bit_debugger):
-    '''
-    Adjust faulting address for instructions that use offsets
-    Currently only works for instructions like CALL [reg + offset]
-    '''
-    if '0x' not in faultaddr:
-        faultaddr = '0x' + faultaddr
-    instructionpieces = instructionline.split()
-    for index, piece in enumerate(instructionpieces):
-        if piece == 'call':
-            # CALL instruction
-            if len(instructionpieces) <= index + 3:
-                # CALL to just a register.  No offset
-                return faultaddr
-            address = instructionpieces[index + 3]
-            if '+' in address:
-                splitaddress = address.split('+')
-                reg = splitaddress[0]
-                reg = reg.replace('[', '')
-                if reg not in reg_set:
-                    return faultaddr
-                offset = splitaddress[1]
-                offset = offset.replace('h', '')
-                offset = offset.replace(']', '')
-                if '0x' not in offset:
-                    offset = '0x' + offset
-                if int(offset, 16) > int(faultaddr, 16):
-                    # TODO: fix up negative numbers
-                    return faultaddr
-                # Subtract offset to get actual interesting pattern
-                faultaddr = hex(eval(faultaddr) - eval(offset))
-                faultaddr = format_addr(faultaddr.replace('L', ''), _64bit_debugger)
-    return faultaddr
-
-
 class LinuxTestCaseBundle(TestCaseBundle):
     really_exploitable = [
                   'SegFaultOnPc',
@@ -204,7 +43,6 @@ class LinuxTestCaseBundle(TestCaseBundle):
                   'BadInstruction',
                   'ReturnAv',
                   ]
-
 
     def _check_64bit(self):
         '''
@@ -275,22 +113,22 @@ class LinuxTestCaseBundle(TestCaseBundle):
         # Set the "fuzzedfile" property for the crash ID
         details['fuzzedfile'] = crasherfile
         faultaddr = carve2(reporttext)
-        instraddr = get_instr_addr(reporttext)
-        faultaddr = format_addr(faultaddr, _64bit_debugger)
-        instraddr = format_addr(instraddr, _64bit_debugger)
+        instraddr = self.get_instr_addr()
+        faultaddr = self.format_addr(faultaddr, _64bit_debugger)
+        instraddr = self.format_addr(instraddr, _64bit_debugger)
 
         # No faulting address means no crash.
         if not faultaddr:
             return
 
         if instraddr:
-            details['exceptions'][exceptionnum]['pcmodule'] = pc_in_mapped_address(reporttext, instraddr)
+            details['exceptions'][exceptionnum]['pcmodule'] = self.pc_in_mapped_address(instraddr)
 
         # Get the cdb line that contains the crashing instruction
-        instructionline = get_instr(reporttext, instraddr)
+        instructionline = self.get_instr(instraddr)
         details['exceptions'][exceptionnum]['instructionline'] = instructionline
         if instructionline:
-            faultaddr = fix_efa_offset(instructionline, faultaddr, _64bit_debugger)
+            faultaddr = self.fix_efa_offset(instructionline, faultaddr)
 
         # Fix faulting pattern endian
         faultaddr = faultaddr.replace('0x', '')
@@ -315,6 +153,163 @@ class LinuxTestCaseBundle(TestCaseBundle):
             details['exceptions'][exceptionnum]['EIF'] = True
         else:
             details['exceptions'][exceptionnum]['EIF'] = False
+
+    def format_addr(self, faultaddr):
+        '''
+        Format a 64- or 32-bit memory address to a fixed width
+        '''
+
+        if not faultaddr:
+            return
+        else:
+            faultaddr = faultaddr.strip()
+        faultaddr = faultaddr.replace('0x', '')
+
+        if self._64bit_debugger:
+            # Due to a bug in !exploitable, the Exception Faulting Address is
+            # often wrong with 64-bit targets
+            if len(faultaddr) < 10:
+                # pad faultaddr
+                faultaddr = faultaddr.zfill(16)
+        else:
+            if len(faultaddr) > 10:  # 0x12345678 = 10 chars
+                faultaddr = faultaddr[-8:]
+            elif len(faultaddr) < 10:
+                # pad faultaddr
+                faultaddr = faultaddr.zfill(8)
+
+        return faultaddr
+
+    def fix_efa_offset(self, instructionline, faultaddr):
+        '''
+        Adjust faulting address for instructions that use offsets
+        Currently only works for instructions like CALL [reg + offset]
+        '''
+        if '0x' not in faultaddr:
+            faultaddr = '0x' + faultaddr
+        instructionpieces = instructionline.split()
+        for index, piece in enumerate(instructionpieces):
+            if piece == 'call':
+                # CALL instruction
+                if len(instructionpieces) <= index + 3:
+                    # CALL to just a register.  No offset
+                    return faultaddr
+                address = instructionpieces[index + 3]
+                if '+' in address:
+                    splitaddress = address.split('+')
+                    reg = splitaddress[0]
+                    reg = reg.replace('[', '')
+                    if reg not in reg_set:
+                        return faultaddr
+                    offset = splitaddress[1]
+                    offset = offset.replace('h', '')
+                    offset = offset.replace(']', '')
+                    if '0x' not in offset:
+                        offset = '0x' + offset
+                    if int(offset, 16) > int(faultaddr, 16):
+                        # TODO: fix up negative numbers
+                        return faultaddr
+                    # Subtract offset to get actual interesting pattern
+                    faultaddr = hex(eval(faultaddr) - eval(offset))
+                    faultaddr = self.format_addr(faultaddr.replace('L', ''))
+        return faultaddr
+
+    def get_instr(self, instraddr):
+        '''
+        Find the disassembly line for the current (crashing) instruction
+        '''
+        rgx = regex['current_instr']
+        for line in self.reporttext.splitlines():
+            n = rgx.match(line)
+            if n:
+                return n.group(3)
+        return ''
+
+    def fix_efa_bug(self, instraddr, faultaddr):
+        '''
+        !exploitable often reports an incorrect EFA for 64-bit targets.
+        If we're dealing with a 64-bit target, we can second-guess the reported EFA
+        '''
+        instructionline = self.get_instr(instraddr)
+        if not instructionline:
+            return faultaddr
+        ds = carve(instructionline, "ds:", "=")
+        if ds:
+            faultaddr = ds.replace('`', '')
+        return faultaddr
+
+    def pc_in_mapped_address(self, instraddr):
+        '''
+        Check if the instruction pointer is in a loaded module
+        '''
+        if not instraddr:
+            # The gdb file doesn't have anything in it that'll tell us
+            # where the PC is.
+            return ''
+    #    print 'checking if %s is mapped...' % instraddr
+        mapped_module = 'unloaded'
+
+        instraddr = int(instraddr, 16)
+    #    print 'instraddr: %d' % instraddr
+        for line in self.reporttext.splitlines():
+            #print 'checking: %s for %s' % (line,regex['mapped_frame'])
+            n = re.search(regex['mapped_frame'], line)
+            if n:
+    #            print '*** found mapped address regex!'
+                # Strip out backticks present on 64-bit systems
+                begin_address = int(n.group(1).replace('`', ''), 16)
+                end_address = int(n.group(2).replace('`', ''), 16)
+                if begin_address < instraddr < end_address:
+                    mapped_module = n.group(4)
+                    #print 'mapped_module: %s' % mapped_module
+            else:
+                # [vdso] still counts as a mapped module
+                n = re.search(regex['vdso'], line)
+                if n:
+                    begin_address = int(n.group(1).replace('`', ''), 16)
+                    end_address = int(n.group(2).replace('`', ''), 16)
+                    if begin_address < instraddr < end_address:
+                        mapped_module = '[vdso]'
+
+        return mapped_module
+
+    def get_ex_num(self):
+        '''
+        Get the exception number by counting the number of continues
+        '''
+        exception = 0
+        for line in self.reporttext.splitlines():
+            n = re.match(regex['dbg_prompt'], line)
+            if n:
+                cdbcmd = n.group(1)
+                cmds = cdbcmd.split(';')
+                for cmd in cmds:
+                    if cmd == 'g':
+                        exception = exception + 1
+        return exception
+
+    def get_instr_addr(self):
+        '''
+        Find the address for the current (crashing) instruction
+        '''
+        instraddr = None
+        for line in self.reporttext.splitlines():
+            #print 'checking: %s' % line
+            n = re.match(regex['current_instr'], line)
+            if n:
+                instraddr = n.group(1)
+                #print 'Found instruction address: %s' % instraddr
+        if not instraddr:
+            for line in self.reporttext.splitlines():
+                #No disassembly. Resort to frame 0 address
+                n = re.match(regex['frame0'], line)
+                if n:
+                    instraddr = n.group(1)
+                    #print 'Found instruction address: %s' % instraddr
+        return instraddr
+
+
+
 
 
 class LinuxResultDriller(ResultDriller):
