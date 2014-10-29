2155c54757f29ddfa5355b3195b258ea92ad9002
Allen D. Householder <adh@cert.org>
2014-10-29 09:07:39 -0400

    add empty unit tests

---
+++ src/certfuzz/test/testcase_pipeline/test_tc_pipeline_base.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 29, 2014
+
+@organization: cert.org
+'''
+import unittest
+import certfuzz.testcase_pipeline.tc_pipeline_base
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testName(self):
+        pass
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
