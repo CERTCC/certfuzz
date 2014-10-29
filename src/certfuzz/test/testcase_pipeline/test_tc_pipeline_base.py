68c45ff9af4154452ee00596645f6c4851ff06e4
Allen D. Householder <adh@cert.org>
2014-10-29 10:42:35 -0400

    add unit test of abstract base class implementation

--- src/certfuzz/test/testcase_pipeline/test_tc_pipeline_base.py
+++ src/certfuzz/test/testcase_pipeline/test_tc_pipeline_base.py
@@ -6,6 +6,24 @@ Created on Oct 29, 2014
 import unittest
 import certfuzz.testcase_pipeline.tc_pipeline_base
 
+
+class TCPL_Impl(certfuzz.testcase_pipeline.tc_pipeline_base.TestCasePipelineBase):
+    def _setup_analyzers(self):
+        pass
+
+    def _minimize(self):
+        pass
+
+    def _verify(self):
+        pass
+
+    def _analyze(self):
+        pass
+
+    def _report(self):
+        pass
+
+
 class Test(unittest.TestCase):
 
     def setUp(self):
@@ -14,8 +32,16 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testName(self):
-        pass
+    def test_abstract_class(self):
+        cls = certfuzz.testcase_pipeline.tc_pipeline_base.TestCasePipelineBase
+
+        # should fail since we haven't implemented the abc methods
+        self.assertRaises(TypeError, cls)
+
+        try:
+            TCPL_Impl()
+        except TypeError as e:
+            self.fail('Dummy implementation class failed to instantiate: {}'.format(e))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
