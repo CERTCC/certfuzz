cfe015eb07e22ba201c345af7ef21fd6bfb8e134
Allen D. Householder <adh@cert.org>
2014-10-29 10:22:34 -0400

    add unit test

--- src/certfuzz/test/helpers/test_coroutine.py
+++ src/certfuzz/test/helpers/test_coroutine.py
@@ -6,6 +6,7 @@ Created on Oct 29, 2014
 import unittest
 import certfuzz.helpers.coroutine
 
+
 class Test(unittest.TestCase):
 
     def setUp(self):
@@ -14,8 +15,28 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testName(self):
-        pass
+    def test_coroutine(self):
+        results = []
+
+        # set up a simple function that multiplies
+        # the values sent to it and appends it to
+        # a list
+        @certfuzz.helpers.coroutine.coroutine
+        def func(results):
+            while True:
+                x = (yield)
+                results.append(2 * x)
+
+        # set up the coroutine
+        c = func(results)
+
+        # confirm that results has the right size and values
+        for i in xrange(100):
+            self.assertEqual(i, len(results))
+            c.send(i)
+            self.assertEqual(i + 1, len(results))
+            self.assertEqual(i * 2, results[-1])
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
