4c590c7addda09f38adbb2bc24bc5b72d0bccbbf
Allen D. Householder <adh@cert.org>
2014-10-29 11:24:07 -0400

    add unit test for pipeline functions

--- src/certfuzz/test/testcase_pipeline/test_tc_pipeline_base.py
+++ src/certfuzz/test/testcase_pipeline/test_tc_pipeline_base.py
@@ -43,6 +43,35 @@ class Test(unittest.TestCase):
         except TypeError as e:
             self.fail('Dummy implementation class failed to instantiate: {}'.format(e))
 
+    def test_pipeline_coroutines(self):
+        tcpl = TCPL_Impl()
+        results = []
+
+        def func(tc):
+            results.append(tc)
+
+        tcpl._verify = func
+        tcpl._minimize = func
+        tcpl._analyze = func
+        tcpl._report = func
+        funcs2check = [tcpl.verify,
+                       tcpl.minimize,
+                       tcpl.analyze,
+                       tcpl.report,
+                       ]
+
+        # if this test works, results will get [0,1,2,...]
+        # because each of the above functions will call
+        # its corresponding _function which will append
+        # i to results
+        for i, plfunc in enumerate(funcs2check):
+            #setup the pipeline coroutine
+            testfunc = plfunc()
+            self.assertEqual(i, len(results))
+            testfunc.send(i)
+            self.assertEqual(i + 1, len(results))
+            self.assertEqual(i, results[-1])
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
