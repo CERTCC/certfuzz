1c3e5ef76352f3a4f3113eb263e9886bdce819d3
Allen D. Householder <adh@cert.org>
2014-10-29 13:30:06 -0400

    add test case for _analyze()

--- src/certfuzz/test/testcase_pipeline/test_tc_pipeline_base.py
+++ src/certfuzz/test/testcase_pipeline/test_tc_pipeline_base.py
@@ -5,32 +5,36 @@ Created on Oct 29, 2014
 '''
 import unittest
 import certfuzz.testcase_pipeline.tc_pipeline_base
+import tempfile
+import shutil
+import os
+import certfuzz.testcase_pipeline.tc_pipeline_base
 
 
 class TCPL_Impl(certfuzz.testcase_pipeline.tc_pipeline_base.TestCasePipelineBase):
     def _setup_analyzers(self):
         pass
 
-    def _minimize(self):
+    def _minimize(self, testcase):
         pass
 
-    def _verify(self):
+    def _verify(self, testcase):
         pass
 
-    def _analyze(self):
+    def _analyze(self, testcase):
         pass
 
-    def _report(self):
+    def _report(self, testcase):
         pass
 
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.tmpdir = tempfile.mkdtemp()
 
     def tearDown(self):
-        pass
+        shutil.rmtree(self.tmpdir)
 
     def test_abstract_class(self):
         cls = certfuzz.testcase_pipeline.tc_pipeline_base.TestCasePipelineBase
@@ -71,6 +75,61 @@ class Test(unittest.TestCase):
             testfunc.send(i)
             self.assertEqual(i + 1, len(results))
             self.assertEqual(i, results[-1])
+
+    def test_copy_files(self):
+        tcpl = TCPL_Impl()
+
+        class MockTestCase(object):
+            result_dir = tempfile.mkdtemp(dir=self.tmpdir)
+
+        tc = MockTestCase()
+        fd, fname = tempfile.mkstemp(dir=tc.result_dir)
+        os.write(fd, fname)
+        os.close(fd)
+
+        self.assertEqual([os.path.basename(fname)],
+                         os.listdir(tc.result_dir))
+        tcpl.outdir = tempfile.mkdtemp(dir=self.tmpdir)
+
+        self.assertEqual([],
+                         os.listdir(tcpl.outdir))
+        tcpl._copy_files(tc)
+        self.assertEqual([os.path.basename(fname)],
+                         os.listdir(tc.result_dir))
+
+    def test_analyze(self):
+        class TCPL_Impl2(TCPL_Impl):
+            def _analyze(self, testcase):
+                certfuzz.testcase_pipeline.tc_pipeline_base.TestCasePipelineBase._analyze(self, testcase)
+
+        tcpl = TCPL_Impl2()
+
+        class MockTestCase(object):
+            pass
+
+        analyzer_count = []
+
+        class MockAnalyzer(object):
+            def __init__(self, *args, **kwargs):
+                pass
+
+            def go(self):
+                analyzer_count.append(1)
+
+        touch_watchdog_call_count = []
+
+        def inc_cc():
+            touch_watchdog_call_count.append(1)
+
+        # monkey patch touch_watchdog_file
+        certfuzz.testcase_pipeline.tc_pipeline_base.touch_watchdog_file = inc_cc
+
+        tc = MockTestCase()
+        tcpl.analyzer_classes = [MockAnalyzer for _ in xrange(5)]
+        tcpl._analyze(tc)
+        self.assertEqual(5, sum(analyzer_count))
+        self.assertEqual(5, sum(touch_watchdog_call_count))
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
