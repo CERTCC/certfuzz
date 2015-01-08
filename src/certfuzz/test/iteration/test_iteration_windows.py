260d66244bc91bbedd8850a1b76ddab7c68f9ed0
Allen D. Householder <adh@cert.org>
2015-01-08 08:54:08 -0500

    fix unit tests

--- src/certfuzz/test/iteration/test_iteration_windows.py
+++ src/certfuzz/test/iteration/test_iteration_windows.py
@@ -11,8 +11,13 @@ from certfuzz.iteration.iteration_windows import WindowsIteration
 class Test(unittest.TestCase):
 
     def setUp(self):
-        args = list('0123456789ABCDE')
-        args[3] = {'runoptions': {'keep_unique_faddr': False}}
+        # args:
+#         seedfile, seednum, config, fuzzer_cls,
+#         runner, debugger, dbg_class, keep_heisenbugs, keep_duplicates,
+#         cmd_template, uniq_func, workdirbase, outdir, debug,
+#         sf_set, rf
+        args = list('0123456789ABCDEF')
+        args[2] = {'runoptions': {'keep_unique_faddr': False}}
         self.iteration = WindowsIteration(*args)
 
     def tearDown(self):
@@ -22,5 +27,5 @@ class Test(unittest.TestCase):
         pass
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
