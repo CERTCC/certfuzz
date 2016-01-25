c3b181928c12324ab7d1b1074ede8b6b3ce0d801
Allen D. Householder <adh@cert.org>
2016-01-25 09:46:58 -0500

    remove get_command_args_list from config_windows (now in fuzztools.comman_line_templating)

--- src/certfuzz/config/config_windows.py
+++ src/certfuzz/config/config_windows.py
@@ -4,7 +4,6 @@ Created on Feb 9, 2012
 @organization: cert.org
 '''
 import logging
-import shlex
 from string import Template
 
 from certfuzz.config.config_base import ConfigBase
@@ -13,21 +12,6 @@ from certfuzz.helpers import quoted
 
 
 logger = logging.getLogger(__name__)
-
-
-def get_command_args_list(cmd_template, infile, posix=True):
-    '''
-    Given a command template and infile, will substitute infile into the
-    template, and return both the complete string and its component parts
-    as returned by shlex.split. The optional posix parameter is passed to
-    shlex.split (defaults to true).
-    :param cmd_template: a string.Template object containing "$SEEDFILE"
-    :param infile: the string to substitute for "$SEEDFILE" in cmd_template
-    :param posix: (optional) passed through to shlex.split
-    '''
-    cmd = cmd_template.substitute(SEEDFILE=infile)
-    cmdlist = shlex.split(cmd, posix=posix)
-    return cmd, cmdlist
 
 
 class WindowsConfig(ConfigBase):
