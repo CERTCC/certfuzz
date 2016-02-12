741b2fac9a20af80ef901d866ccd7eba7718e2dc
Will Dormann <wd@cert.org>
2016-02-12 13:42:19 -0500

    More verbose logging of symlink script

--- build/distmods/osx/installer/scripts/symlink.sh
+++ build/distmods/osx/installer/scripts/symlink.sh
@@ -5,20 +5,20 @@ bffdir="$installdir/bff"
 homedir=`cd && pwd`
 
 if [ ! -e $homedir/bff ]; then
-  ln -s $bffdir $homedir/bff
+  ln -s $bffdir $homedir/bff 2>> /tmp/symlink.err
 fi
 
 if [ -L ~/results ]; then
-  rm $homedir/results
-  mkdir $homedir/results
+  rm $homedir/results 2>> /tmp/symlink.err
+  mkdir $homedir/results 2>> /tmp/symlink.err
 fi
 
 if [ ! -e $homedir/results ]; then
-  mkdir $homedir/results
-  chmod g+w $homedir/results
+  mkdir $homedir/results 2>> /tmp/symlink.err
+  chmod g+w $homedir/results 2>> /tmp/symlink.err
 fi
 
-rmdir $homedir/bff/results 2> /tmp/rm.err
-ln -s $homedir/results $homedir/bff/results 2> /tmp/ln.err
+rmdir $homedir/bff/results 2>> /tmp/symlink.err
+ln -s $homedir/results $homedir/bff/results 2>> symlink.err
 
 exit 0
