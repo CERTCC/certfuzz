bea5d8ccf28a94436b287e2576b3d5720e5dd214
Will Dormann <wd@cert.org>
2016-02-15 01:20:01 -0500

    Make symlinks after installing bff files, not before.  BFF-464

--- build/distmods/osx/installer/scripts/chmod.sh
+++ build/distmods/osx/installer/scripts/chmod.sh
@@ -3,6 +3,24 @@
 installdir="$2"
 bffdir="$installdir/bff"
 packagesdir="$2/packages"
+homedir=`cd && pwd`
+
+if [ ! -e $homedir/bff ]; then
+  ln -s $bffdir $homedir/bff
+fi
+
+if [ -L ~/results ]; then
+  rm $homedir/results
+  mkdir $homedir/results
+fi
+
+if [ ! -e $homedir/results ]; then
+  mkdir $homedir/results
+  chmod g+w $homedir/results
+fi
+
+rmdir $homedir/bff/results
+ln -s $homedir/results $homedir/bff/results
 
 launchctl unload -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
 sed "s/^copymode=0/copymode=1/" $bffdir/conf.d/bff.cfg > $bffdir/conf.d/cfg.tmp
