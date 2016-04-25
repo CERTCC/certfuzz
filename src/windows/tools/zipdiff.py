f2d224a772227ec3dd85cb8ec152d319cd97638f
Will Dormann <wd@cert.org>
2016-04-25 13:59:03 -0400

    zipdiff moved from "windows" to "common"

--- src/windows/tools/zipdiff.py
+++ src/windows/tools/zipdiff.py
@@ -6,13 +6,13 @@ Created on Jul 10, 2013
 import os
 import sys
 try:
-    from certfuzz.tools.windows.zipdiff import main
+    from certfuzz.tools.common.zipdiff import main
 except ImportError:
     # if we got here, we probably don't have .. in our PYTHONPATH
     mydir = os.path.dirname(os.path.abspath(__file__))
     parentdir = os.path.abspath(os.path.join(mydir, '..'))
     sys.path.append(parentdir)
-    from certfuzz.tools.windows.zipdiff import main
+    from certfuzz.tools.common.zipdiff import main
 
 if __name__ == '__main__':
     main()
