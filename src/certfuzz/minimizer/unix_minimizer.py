e72b021d8a1ea860c4c0fb7e688e54a675d2078b
Will Dormann <wd@cert.org>
2016-04-28 11:14:41 -0400

    Touch watchdog file during minimization.  BFF-941

--- src/certfuzz/minimizer/unix_minimizer.py
+++ src/certfuzz/minimizer/unix_minimizer.py
@@ -20,24 +20,21 @@ class UnixMinimizer(MinimizerBase):
     def __enter__(self):
         # touch the watchdogfile
         try:
-            open(self.cfg.watchdogfile, 'w').close()
+            open(self.watchdogfile, 'w').close()
         except (OSError, IOError) as e:
             # it's okay if we can't, but we should note it
             self.logger.warning('Unable to touch watchdog file %s: %s',
-                                self.cfg.watchdogfile, e)
-        except AttributeError:
-            # self.cfg doesn't have a watchdogfile
-            self.logger.info('Config has no watchdogfile attribute, skipping')
+                                self.watchdogfile, e)
 
         return MinimizerBase.__enter__(self)
 
     def __exit__(self, etype, value, traceback):
         try:
-            os.remove(self.cfg.watchdogfile)
+            os.remove(self.watchdogfile)
         except (OSError, IOError) as e:
             # it's okay if we can't, but we should note it
             self.logger.warning('Unable to remove watchdog file %s: %s',
-                                self.cfg.watchdogfile, e)
+                                self.watchdogfile, e)
         except AttributeError:
             # self.cfg doesn't have a watchdogfile
             # We probably already logged this fact in __enter__ so we can
