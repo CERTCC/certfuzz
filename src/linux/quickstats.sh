000f5e346cdd92ec78fcfced010f4e9cdce7eb6e
Will Dormann <wd@cert.org>
2016-06-16 14:02:46 -0400

    Fix quickstats.sh count on OSX

--- src/linux/quickstats.sh
+++ src/linux/quickstats.sh
@@ -4,8 +4,7 @@ platform=`uname -a`
 echo "Exploitability Summary for campaign thus far:"
 if [[ "$platform" =~ "Darwin" ]]; then
 	exploitable=`find -L ~/results -name '*.cw' | xargs grep is_exploitable=y | awk -Fcrashers/ '{print $2}' | awk -F/ '{print $1}' | sort | uniq | wc -l`
-	total=`find -L ~/results -name '*.cw' | wc -l`
-	let total-=1
+	total=`find -L ~/results -name '*.cw' | grep -v gmalloc | wc -l`
 	not_exploitable=$(expr $total - $exploitable)
 	echo 	$exploitable Exploitable
 	echo 	$not_exploitable Unknown
