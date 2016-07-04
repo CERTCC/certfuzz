b049493c76af5bee6b537707eedb7d3ed9e8b3a6
Will Dormann <wd@cert.org>
2016-07-04 11:10:16 -0400

    Make sure quickstats.sh is only looking in the crashers directory

--- src/linux/quickstats.sh
+++ src/linux/quickstats.sh
@@ -3,13 +3,13 @@
 platform=`uname -a`
 echo "Exploitability Summary for campaign thus far:"
 if [[ "$platform" =~ "Darwin" ]]; then
-	exploitable=`find -L ~/results -name '*.cw' | xargs grep is_exploitable=y | awk -Fcrashers/ '{print $2}' | awk -F/ '{print $1}' | sort | uniq | wc -l`
-	total=`find -L ~/results -name '*.cw' | grep -v gmalloc | wc -l`
+	exploitable=`find -L ~/results -name '*.cw' | grep crashers | xargs grep is_exploitable=y | awk -Fcrashers/ '{print $2}' | awk -F/ '{print $1}' | sort | uniq | wc -l`
+	total=`find -L ~/results -name '*.cw' | grep crashers | grep -v gmalloc | wc -l`
 	not_exploitable=$(expr $total - $exploitable)
 	echo 	$exploitable Exploitable
 	echo 	$not_exploitable Unknown
     echo    $total Total
 else
-	find -L ~/results -name '*.gdb' | xargs grep -h "Exploitability Classification" | cut -d" " -f3 | sort | uniq -c
-    echo `find -L ~/results -name '*.gdb' | wc -l` TOTAL
+	find -L ~/results -name '*.gdb' | grep crashers | xargs grep -h "Exploitability Classification" | cut -d" " -f3 | sort | uniq -c
+    echo `find -L ~/results -name '*.gdb' | grep crashers | wc -l` TOTAL
 fi
