13b4d21764efe092f56f76d14cabb594732acb60
Will Dormann <wd@cert.org>
2016-07-11 16:15:26 -0400

    Add stub unit tests for new modules.

---
+++ src/test_certfuzz/drillresults/test_result_driller_darwin.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 23, 2014
+
+@organization: cert.org
+'''
+import unittest
+from certfuzz.drillresults import result_driller_darwin
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testName(self):
+        pass
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
