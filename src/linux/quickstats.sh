0721af8fa12d2783733f1db9601da8c58fa49e87
Will Dormann <wd@cert.org>
2016-08-02 11:48:24 -0400

    Use ~/bff/results instead of ~/results

--- src/linux/quickstats.sh
+++ src/linux/quickstats.sh
@@ -18,13 +18,13 @@ contains() {
 platform=`uname -a`
 echo "Exploitability Summary for campaign thus far:"
 if ( contains "$platform" "Darwin" ); then
-	exploitable=`find -L ~/results -name '*.cw' | grep crashers | xargs grep is_exploitable=y | awk -Fcrashers/ '{print $2}' | awk -F/ '{print $1}' | sort | uniq | wc -l`
-	total=`find -L ~/results -name '*.cw' | grep crashers | grep -v gmalloc | wc -l`
+	exploitable=`find -L ~/bff/results -name '*.cw' | grep crashers | xargs grep is_exploitable=y | awk -Fcrashers/ '{print $2}' | awk -F/ '{print $1}' | sort | uniq | wc -l`
+	total=`find -L ~/bff/results -name '*.cw' | grep crashers | grep -v gmalloc | wc -l`
 	not_exploitable=$(expr $total - $exploitable)
 	echo 	$exploitable Exploitable
 	echo 	$not_exploitable Unknown
     echo    $total Total
 else
-	find -L ~/results -name '*.gdb' | grep crashers | xargs grep -h "Exploitability Classification" | cut -d" " -f3 | sort | uniq -c
-    echo `find -L ~/results -name '*.gdb' | grep crashers | wc -l` TOTAL
+	find -L ~/bff/results -name '*.gdb' | grep crashers | xargs grep -h "Exploitability Classification" | cut -d" " -f3 | sort | uniq -c
+    echo `find -L ~/bff/results -name '*.gdb' | grep crashers | wc -l` TOTAL
 fi
