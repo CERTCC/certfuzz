e12c041fcc761ddf8c62dd0cc05ed1ba523e689f
Will Dormann <wd@cert.org>
2016-09-02 11:22:28 -0400

    stub unit test

---
+++ src/test_certfuzz/tools/common/test_updatebff.py
@@ -0,0 +1,27 @@
+'''
+Created on Apr 10, 2012
+
+@organization: cert.org
+'''
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
